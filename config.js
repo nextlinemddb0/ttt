9a397e9c7c94395f9301a90a8feab442ab757714
nextlinemddb0 <Nextlinemd@gmail.com>
2026-07-07 20:21:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 //gg
 module.exports = {
-SESSION_ID:'edNClIiR#NjOSq6hXRBiAWVP9ekvh2Sa0gBrQELLyKslKuVrZRBM',
+SESSION_ID:'fQMEnIbS#QZdvZ3_WxKbyYkKIlK6vXEQ-Uuod1lCP0oygsiXj5_I',
 
 ANTI_DELETE: process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE, 
 MV_BLOCK: process.env. MV_BLOCK === undefined ? 'true' : process.env. MV_BLOCK,    
